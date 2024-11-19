8518858374947def71789b443b7456bc4addbd7c
Dan <danial.horne@yahoo.com>
2024-11-19 21:38:24 +0000

    Working glob==regex check against files in .ftpignore

--- upload.sh
+++ upload.sh
@@ -16,39 +16,66 @@ done
 
 TARGET_DIR="."
 
-shopt -s extglob
+IGNORED_PATTERNS+=("$@")
 
-IGNORED_PATTERNS=($1)
+glob_to_regex() {
+    local glob="$1"
+    local regex="^${glob//./\\.}"
+    regex="${regex//\*/.*}"
+    regex="${regex//\?/.}"
+    regex="${regex//\[\\!/[^\]}"
+    echo "$regex"
+}
+
+for pattern in "${IGNORED_PATTERNS[@]}"; do
+    REGEX_PATTERNS+=("$(glob_to_regex "$pattern")")
+done
+
+shopt -s extglob
 
 for file in "$TARGET_DIR"/*; do
     filename=$(basename "$file")
     matched=false
-    if [[ $filename =~ .* ]]; then 
-        echo "checking $filename";
-    else 
-        echo "not checking"; 
-    fi
 
-    for pattern in "${IGNORED_PATTERNS[@]}"; do
-        echo "checking against $pattern"
-        # Handle negated patterns (!pattern)
-        if [[ $pattern == \!* ]]; then
-            negated_pattern=${pattern:1}  # Remove the '!'
-            if [[ $filename =~ $negated_pattern ]]; then
-                matched=false
-                break
-            fi
-        elif [[ $filename =~ $pattern ]]; then
+    for regex in "${REGEX_PATTERNS[@]}"; do
+        if [[ $filename =~ $regex ]]; then
             matched=true
-            echo "$filename matches $pattern";
-            break;
+            echo "$filename matches $regex, take two electric boogaloo"
+            break
         fi
     done
 
-    # Print the matching files
     if $matched; then
-        echo "$filename matches a pattern in $IGNORE_FILE"
+        echo "$filename matches pattern"
     fi
 done
+
+#     if [[ $filename =~ .* ]]; then 
+#         echo "checking $filename";
+#     else 
+#         echo "not checking"; 
+#     fi
+
+#     for pattern in "${IGNORED_PATTERNS[@]}"; do
+#         echo "checking against $pattern"
+#         # Handle negated patterns (!pattern)
+#         if [[ $pattern == \!* ]]; then
+#             negated_pattern=${pattern:1}  # Remove the '!'
+#             if [[ $filename =~ $negated_pattern ]]; then
+#                 matched=false
+#                 break
+#             fi
+#         elif [[ $filename =~ $pattern ]]; then
+#             matched=true
+#             echo "$filename matches $pattern";
+#             break;
+#         fi
+#     done
+
+#     Print the matching files
+#     if $matched; then
+#         echo "$filename matches a pattern in $IGNORE_FILE"
+#     fi
+# done
 
 shopt -u extglob
